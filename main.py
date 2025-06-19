b8da76577ee20ca1c4feaf6df15fe703ea4222d7
23fi123 <hanikitifoto@gmail.com>
2025-06-19 12:37:39 +0900

    main.py

--- main.py
+++ main.py
@@ -49,4 +49,4 @@ def index():
 
 @app.post("/present")
 async def give_present(present):
-    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  # f文字列というPythonの機能を使っている
+    return {"response": f"サーバです。誕生日おめでとう私！ {present}ありがとう。この日を忘れたりはしません。"}  # f文字列というPythonの機能を使っている
