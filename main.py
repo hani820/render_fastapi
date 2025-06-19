d0d792064471177749d9c3c4426fbbba8f61665b
23fi123 <hanikitifoto@gmail.com>
2025-06-19 12:27:38 +0900

    main.py

--- main.py
+++ main.py
@@ -2,8 +2,34 @@ from fastapi.responses import HTMLResponse #インポート
 
 from fastapi import FastAPI
 
+import random  # randomライブラリを追加
+
 app = FastAPI()
-### コードいろいろ... ###
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/omikuji")
+def omikuji():
+    omikuji_list = [
+        "大吉",
+        "中吉",
+        "小吉",
+        "吉",
+        "半吉",
+        "末吉",
+        "末小吉",
+        "凶",
+        "小凶",
+        "大凶"
+    ]
+    
+    return {"result" : omikuji_list[random.randrange(10)]}
 
 @app.get("/index")
 def index():
@@ -20,3 +46,7 @@ def index():
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
+
+    @app.post("/present")
+async def give_present(present):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  # f文字列というPythonの機能を使っている
